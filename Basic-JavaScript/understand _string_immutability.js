16d1c08f575ff662594a917ba058aab987f61ebf
Matias Jesus Alicata <matiasalicata@gmail.com>
2022-06-15 11:19:53 -0300

    15062022
    basic javascript notes

---
+++ Basic-JavaScript/understand _string_immutability.js
@@ -0,0 +1,8 @@
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+// myStr[0] = "H"; Change this line
+myStr = "Hello World";
+console.log(myStr);
+// Only change code above this line
